27b7d3a77c346467e5daa4dd19ac8761c0873a29
marvelousakinyemi910-commits <marvelousakinyemi910@gmail.com>
2026-03-26 10:35:34 +0100

    Remove Duplicates

---
+++ java/GateEight/RemoveDuplicate.java
@@ -0,0 +1,35 @@
+public class RemoveDuplicate{
+
+    public [] removeDuplicates (int [] numbers){
+            int count = 0;
+           
+        for(int rowIndex = 0; rowIndex < numbers.length; rowIndex++){
+                int smallest = numbers[rowIndex];
+                for (int columnIndex = rowIndex +1; columnIndex < numbers.length; columnIndex){
+                        
+                        if(numbers[columnIndex] < smallest){
+                            smallest = numbers[columnIndex];
+                            numbers[columnIndex] = numbers[rowIndex];
+                            numbers[rowIndex] = smallest;
+                        }
+
+                    }
+
+
+        }
+                int counter = 1;
+                for(int index = 1; index < numbers.length; index++){
+                    if(numbers[index] != numbers[index -1]){
+                    counter++;
+                    }
+
+
+            }
+            
+            int [] newNumber = new int [counter];
+              
+                
+            
+    }
+
+}
